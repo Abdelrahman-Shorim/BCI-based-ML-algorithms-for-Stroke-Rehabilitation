ab085e4b4c109de9774e59d3b59dfe8016fb2813
HabibaMohamed07 <Habiba2003520@miuegypt.edu.eg>
2024-04-09 00:08:57 +0200

    FrontEndChanges

--- Frontend/src/LandingPage/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Article } from './article/Article';
-export { default as Brand } from './brand/Brand';
-export { default as CTA } from '../frontpage/cta/CTA';
-export { default as Feature } from '../frontpage/feature/Feature';
-export { default as Navbar } from './navbar/Navbar';
